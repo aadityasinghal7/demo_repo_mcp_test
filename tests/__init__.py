41a4867dbbbfd86ad088e18b3a74185141217635
aadityasinghal7 <aaditya_singhal2018@pgp_isb_edu>
2025-08-25 11:56:50 -0400

    Remove test folder

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# due to our project requiign blah blah blah,
-# we will test this stuff i this way and rely on open source for this stuff.
-# we will have e2e tests mimicing this science workflow
--- tests/test_math_utils.py
+++
@@ -1,4 +0,0 @@
-from src.math_utils import add
-
-def test_add():
-    assert add(2, 3) == 5
