33a2755d26a439c1b323664edf020cbdd9667d13
leedtan <leedtan@gmail.com>
2025-09-07 11:57:23 -0400

    Add test file: tests/test_data.py with test: test_weigthed_average_zero_total_weight

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pytest
+
+from src.data.data_processing import weigthed_average
+
+
+def test_weigthed_average_zero_total_weight():
+    df = pd.DataFrame(
+        {
+            "A": [1, 2, 3],
+            "B": [4, 5, 6],
+        }
+    )
+    weights = {"A": 0, "B": 0}
+
+    with pytest.raises(ZeroDivisionError):
+        _ = weigthed_average(df, weights)
